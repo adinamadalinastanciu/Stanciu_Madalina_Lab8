928ae9b6901e3393a6fcbb4f60f8595cab9beea3
Madalina Stanciu <stanciuserviciu@yahoo.com>
2020-12-06 23:42:04 +0200

    first commit

---
+++ Data/Stanciu_Madalina_Lab8Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Stanciu_Madalina_Lab8.Models;
+
+namespace Stanciu_Madalina_Lab8.Data
+{
+    public class Stanciu_Madalina_Lab8Context : DbContext
+    {
+        public Stanciu_Madalina_Lab8Context (DbContextOptions<Stanciu_Madalina_Lab8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Stanciu_Madalina_Lab8.Models.Book> Book { get; set; }
+
+        public DbSet<Stanciu_Madalina_Lab8.Models.Publisher> Publisher { get; set; }
+
+        public DbSet<Stanciu_Madalina_Lab8.Models.Category> Category { get; set; }
+    }
+}
---
+++ Pages/Categories/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Stanciu_Madalina_Lab8.Data;
+using Stanciu_Madalina_Lab8.Models;
+
+namespace Stanciu_Madalina_Lab8.Pages.Categories
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Stanciu_Madalina_Lab8.Data.Stanciu_Madalina_Lab8Context _context;
+
+        public IndexModel(Stanciu_Madalina_Lab8.Data.Stanciu_Madalina_Lab8Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Category> Category { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Category = await _context.Category.ToListAsync();
+        }
+    }
+}
